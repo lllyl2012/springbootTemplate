50ca62c6488448d9c2d9c084d9b61c5a1714a6ce
lllyl2012 <229707363@qq.com>
2018-03-10 08:30:46 +0800

    second

--- src/main/java/com/hpit/test/util/SpringUtil.java
+++ src/main/java/com/hpit/test/util/SpringUtil.java
@@ -12,7 +12,7 @@ public class SpringUtil implements ApplicationContextAware {
 	private static ApplicationContext applicationContext;
 
 	@Override
-	public void setApplicationContext(ApplicationContext arg0)
+	public void setApplicationContext(ApplicationContext applicationContext)
 			throws BeansException {
 		if (SpringUtil.applicationContext == null) {
 			SpringUtil.applicationContext = applicationContext;
